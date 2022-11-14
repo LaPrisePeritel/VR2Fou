5767612d5cbbcc294a706613771bff77b8bac984
Zeldruck <felix.jouannneau@gmail.com>
2022-11-14 19:15:28 +0100

    Aveugle

---
+++ VR2Fou/Assets/Scripts/Alien.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    private Action onTouchBorder;
+
+    private Vector3 leftBorder, rightBorder;
+    
+    public void Initialisation(Action _onTouchBorder, Vector3 _leftBorder, Vector3 _rightBorder)
+    {
+        onTouchBorder = _onTouchBorder;
+        leftBorder = _leftBorder;
+        rightBorder = _rightBorder;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x + transform.localScale.x / 2f >= rightBorder.x)
+        {
+            onTouchBorder();
+        }
+        else if (transform.position.x - transform.localScale.x / 2f <= leftBorder.x)
+        {
+            onTouchBorder();
+        }
+    }
+}
---
+++ VR2Fou/Assets/Scripts/BoardMovement.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+
+public class BoardMovement : MonoBehaviour
+{
+    private Alien[,] aliens;
+
+    private bool reset;
+    
+    private Vector3 startPosition;
+    private float moveDirection;
+    
+    
+    [Header("Initialization")] 
+    [SerializeField] private Alien aliensPrefab;
+    [SerializeField] private float spaceBetweenAliens;
+    [SerializeField] private int rows, columns;
+    [Space]
+    [SerializeField] private float leftBorderOffset, rightBorderOffset;
+    
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+
+        moveDirection = Vector3.Distance(transform.position, transform.position + Vector3.left * leftBorderOffset) >
+                        Vector3.Distance(transform.position, transform.position + Vector3.right * rightBorderOffset)
+            ? -1f
+            : 1f;
+        
+        aliens = new Alien[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector3 alienPosition = transform.position + Vector3.down * spaceBetweenAliens * i +
+                                        Vector3.right * spaceBetweenAliens * j;
+                aliens[i, j] = Instantiate(aliensPrefab, alienPosition, Quaternion.identity);
+                aliens[i, j].Initialisation(ChangeDirection, startPosition + Vector3.left * leftBorderOffset, startPosition + Vector3.right * rightBorderOffset);
+                aliens[i, j].transform.parent = transform;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.right * moveDirection * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        reset = true;
+    }
+
+    private void ChangeDirection()
+    {
+        if (!reset)
+            return;
+
+        reset = false;
+        moveDirection *= -1f;
+        transform.position += Vector3.down * spaceBetweenAliens * 2;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(startPosition + Vector3.left * leftBorderOffset, startPosition + Vector3.right * rightBorderOffset);
+        
+        Gizmos.DrawWireSphere(startPosition + Vector3.left * leftBorderOffset, 0.5f);
+        Gizmos.DrawWireSphere(startPosition + Vector3.right * rightBorderOffset, 0.5f);
+    }
+}
