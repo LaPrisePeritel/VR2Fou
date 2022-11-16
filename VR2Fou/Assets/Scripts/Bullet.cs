5183d77644fbb6ce587c34ed7a775ddd660e8a05
unknown <jules.fritz@edu.devinci.fr>
2022-11-16 10:50:30 +0100

    git commit push pull rebase

--- VR2Fou/Assets/Scripts/Bullet.cs
+++ VR2Fou/Assets/Scripts/Bullet.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private const float LIFETIME = 5.0f;
+    private const float CAMERA_SHAKE_DURATION = 1.0f;
+    private const float CAMERA_SHAKE_MAGNITUDE = 3.0f;
+
     public Vector3 direction;
+
+    private CameraShake cameraShake;
 
     private void Start()
     {
-        Destroy(gameObject, 5.0f);
+        Destroy(gameObject, LIFETIME);
     }
-    public void Initiate(Vector3 initPos, Vector3 dir)
+
+    public void Initiate(Camera camera, Vector3 dir, Vector3 initPos)
     {
+        cameraShake = camera.GetComponent<CameraShake>();
         transform.position = initPos;
         direction = dir;
     }
-    void Update()
+
+    private void Update()
     {
         transform.Translate(direction);
     }
+
     private void OnCollisionEnter(Collision c)
     {
         Debug.Log(c.gameObject.name);
         if (c.collider.CompareTag("Alien"))
         {
+            cameraShake.LaunchShake(CAMERA_SHAKE_DURATION, CAMERA_SHAKE_MAGNITUDE);
             Destroy(c.gameObject);
             Destroy(gameObject);
         }
