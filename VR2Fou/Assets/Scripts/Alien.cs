e3924f36b21d392734a236a59e40939ea66e292f
Alex Psychameron Copin <toretankskilled@gmail.com>
2022-11-18 10:53:33 +0100

    Fix the fix

--- VR2Fou/Assets/Scripts/Alien.cs
+++ VR2Fou/Assets/Scripts/Alien.cs
@@ -222,7 +222,7 @@ public class Alien : MonoBehaviour
             meshRenderer.material.SetFloat("_Effect", value);
         }
 
-        Camera.main.GetComponent<CameraShake>().LaunchShake();
+        Camera.main.GetComponent<CameraShake>().LaunchShake(0.5f, 0.3f);
         Destroy(gameObject);
     }
 
--- VR2Fou/Assets/Scripts/Shooting.cs
+++ VR2Fou/Assets/Scripts/Shooting.cs
@@ -3,16 +3,9 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    private Camera mainCamera;
-
     [SerializeField]
     private List<Bullet> prefabsBullet;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        mainCamera = Camera.main;
-    }
 
     // Update is called once per frame
     private void Update()
@@ -20,7 +13,7 @@ public class Shooting : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             Bullet b = Instantiate(prefabsBullet[Random.Range(0, prefabsBullet.Count)]);
-            b.Initiate(mainCamera, transform.up, transform.position);
+            b.Initiate(transform.up, transform.position);
         }
     }
 }
